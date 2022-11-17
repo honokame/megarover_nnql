1e796b42a140ce41ec96012f302de20578862ae4
honokame <ha199016@g.u-fukui.ac.jp>
2022-11-17 14:58:04 +0900

    位置のランダム指定、角度取得

--- nnql/scripts/nnql.py
+++ nnql/scripts/nnql.py
@@ -4,12 +4,15 @@
 import numpy as np
 
 import rospy
+import tf
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.srv import GetModelState
 from gazebo_msgs.msg import ModelState
 
+pos = [[0,0],[1,0],[0,1],[1,1],[2,1],[1,2],[2,2]]
+
 def set_model(name, x, y, yaw): #指定位置にセット
-  rospy.init_node('nnql') #ノードの初期化
+  #rospy.init_node('nnql') #ノードの初期化
   
   model_state = ModelState()
   model_state.model_name = name
@@ -44,6 +47,23 @@ def get_model(name):
     rospy.loginfo("Service did not process request: "+str(exc))
   return get_state
 
+def get_status(name):
+  model_state = get_model(name)
+  x = round(model_state.pose.position.x) #roundにするかは要検討
+  y = round(model_state.pose.position.y)
+  q1 = model_state.pose.orientation.x
+  q2 = model_state.pose.orientation.y 
+  q3 = model_state.pose.orientation.z
+  q4 = model_state.pose.orientation.w
+  eular = tf.transformations.euler_from_quaternion((q1, q2, q3, q4))
+  yaw = round(eular[2]*180/np.pi)
+  print(x, y, yaw)
+
 if __name__ == '__main__':
-   set_model('vmegarover', 0, 0, np.random.randint(0,361))
-   print(get_model('vmegarover'))
+   rospy.init_node('nnql') #ノードの初期化
+   #np.random.shuffle(pos)
+   #x, y = pos[0] 
+   x, y = pos[np.random.randint(0, len(pos))]
+   set_model('vmegarover', x, y, np.random.randint(0,361))
+   #set_model('vmegarover', 0, 0, -90)
+   get_status('vmegarover')
