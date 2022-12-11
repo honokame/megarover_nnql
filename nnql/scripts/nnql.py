c1d576cb8e42c737988847c33e1f3e3b785b8ae4
honokame <ha199016@g.u-fukui.ac.jp>
2022-12-12 03:55:06 +0900

    csv保存できるようにした

--- nnql/scripts/nnql.py
+++ nnql/scripts/nnql.py
@@ -6,7 +6,8 @@ import numpy as np
 import rospy
 import tf
 import sys #引数
-import os #ファイル関連
+import os #ファイル読み書き
+import csv #リストをcsvに書き込み
 from subprocess import *
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.srv import GetModelState
@@ -59,13 +60,18 @@ def get_status(name):
   q4 = model_state.pose.orientation.w
   eular = tf.transformations.euler_from_quaternion((q1, q2, q3, q4))
   yaw = round(eular[2]*180/np.pi)
-  print(str(name),x, y, yaw)
-  print(str(name),int(x),int(y),round(yaw))
+  #print(str(name),x, y, yaw)
+  #print(str(name),int(x),int(y),round(yaw))
+  rospy.loginfo('x:%d, y:%d, yaw:%d',int(x),int(y),int(yaw))
+  status = str(int(x)) + ',' + str(int(y)) + ',' + str(int(yaw)) + '\n'
+  f_status.write(status)
 
 def get_scan():
   msg = rospy.wait_for_message('/scan', LaserScan)
-  scan = msg.ranges 
-  print(scan[1]) 
+  scan = msg.ranges
+  writer = csv.writer(f_scan)
+  writer.writerow(scan)
+  #print(scan[1])
 
 def action():
   p = call(['rosrun','wall_follower','wall_follower'])
@@ -78,18 +84,22 @@ if __name__ == '__main__':
    status_csv = 'status_' + args[1] + '.csv'
    scan_csv = 'scan_' + args[1] + '.csv'
    f_status = open(status_csv,'w')
+   rospy.loginfo('create %s',status_csv)
    f_scan = open(scan_csv,'w')
-   #f.close()
+   rospy.loginfo('create %s',scan_csv)
    #np.random.shuffle(pos)
    #x, y = pos[0] 
    start_x, start_y = pos_start[np.random.randint(0, len(pos_start))]
+   start_yaw = np.random.randint(0,361)
    goal_x, goal_y = pos_goal[np.random.randint(0, len(pos_goal))]
-   #set_model('vmegarover', start_x, start_y, 0, np.random.randint(0,361))
+   set_model('vmegarover', start_x, start_y, 0, start_yaw)
+   rospy.loginfo('init x:%d, y:%d, yaw:%d',start_x,start_y,start_yaw)
    #set_model('target', goal_x, goal_y, 0.01, 0)
    #set_model('vmegarover', 0, 0, -90)
    #get_status('vmegarover')
    #get_status('target')
    for i in range(100):
+     rospy.loginfo('%d',i)
      action()
      get_scan()
      get_status('vmegarover')
