b58af9dc2af4f77dd8b7e9912d44629fdd0171a7
honokame <honoka.happy.0613@icloud.com>
2023-01-21 15:48:37 +0900

    汎用性をもたせた

--- nnql/scripts/gcn.py
+++ nnql/scripts/gcn.py
@@ -15,15 +15,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #訓練データ読み込み
-train_data = np.loadtxt(fname="data/train37_feature.csv",  dtype="int",  delimiter=",")
-train_place = np.loadtxt(fname="data/train37_statusclass.csv", dtype="int", delimiter=",")
+train_data = np.loadtxt(fname="data/r5t8/train34_feature.csv",  dtype="int",  delimiter=",")
+train_place = np.loadtxt(fname="data/r5t8/train34_statusclass.csv", dtype="int", delimiter=",")
 
 #訓練データグラフ作成
 trainset = []
 for data, place in zip(train_data, train_place):
-  label = place[6]#5,6,7                            # ground truth
-  data = data.tolist() 
-  feature_list = [data[1:73],data[73:145],data[145:217],data[217:289],data[289:361],data[361:433],data[433:505],data[505:577],data[577:649]]
+  label = place[7]-1#5,6,7                            # ground truth
+  data = data.tolist()  
+  #feature_list = [data[1:73],data[73:145],data[145:217],data[217:289],data[289:361],data[361:433],data[433:505],data[505:577],data[577:649]] #r9t8 
+  #feature_list = [data[1:109],data[109:217],data[217:325],data[325:433],data[433:541],data[541:649],data[649:757],data[757:865],data[865:973]] #r9t12
+  feature_list = [data[1:41],data[41:81],data[81:121],data[121:161],data[161:201],data[201:241],data[241:281],data[281:321],data[321:361]] #r5t8 
   #print(feature_list)
   #print(label)
   g = dgl.graph(([0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8], [1,0,2,1,3,2,4,3,5,4,6,5,7,6,8,7]))      # edge
@@ -36,15 +38,17 @@ print("train_data:",len(trainset))
 #plt.show()
 
 #テストデータ読み込み
-test_data = np.loadtxt(fname="data/test1_feature.csv",  dtype="int",  delimiter=",")
-test_place = np.loadtxt(fname="data/test1_statusclass.csv", dtype="int", delimiter=",")
+test_data = np.loadtxt(fname="data/r5t8/test1_feature.csv",  dtype="int",  delimiter=",")
+test_place = np.loadtxt(fname="data/r5t8/test1_statusclass.csv", dtype="int", delimiter=",")
 
 #テストデータグラフ作成
 testset = []
 for data, place in zip(test_data, test_place):
-  label = place[6]                            # ground truth
+  label = place[7]-1                            # ground truth
   data = data.tolist() 
-  feature_list = [data[1:73],data[73:145],data[145:217],data[217:289],data[289:361],data[361:433],data[433:505],data[505:577],data[577:649]]
+  #feature_list = [data[1:73],data[73:145],data[145:217],data[217:289],data[289:361],data[361:433],data[433:505],data[505:577],data[577:649]] #r9t8 
+ # feature_list = [data[1:109],data[109:217],data[217:325],data[325:433],data[433:541],data[541:649],data[649:757],data[757:865],data[865:973]]#r9t12
+  feature_list = [data[1:41],data[41:81],data[81:121],data[121:161],data[161:201],data[201:241],data[241:281],data[281:321],data[321:361]] #r5t8  
   #print(feature_list)
   #print(label)
   g = dgl.graph(([0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8], [1,0,2,1,3,2,4,3,5,4,6,5,7,6,8,7]))      # edge
@@ -125,8 +129,8 @@ data_loader = DataLoader(
     shuffle=True,
     collate_fn=collate)
 
-feature = 72
-classes = 209 #27,209,313
+feature = 40 #r9t12:108 r9t8:72 r5:t8
+classes =312  #26,208,312
 
 # Create model
 model = Classifier(feature, 256, classes)    #特徴ベクトルの次元、場所クラス数
@@ -159,11 +163,11 @@ probs_Y = torch.softmax(model(test_bg), 1)
 sampled_Y = torch.multinomial(probs_Y, 1)
 argmax_Y = torch.max(probs_Y, 1)[1].view(-1, 1)
 
-f=open('data/result34.txt','w')
-for arg in argmax_Y:
-  f.write(str(arg))
-  f.write('\n')
-f.close()
+#f=open('data/result34.txt','w')
+#for arg in argmax_Y:
+#  f.write(str(arg))
+#  f.write('\n')
+#f.close()
 
 print('Accuracy of sampled predictions on the test set: {:.4f}%'.format(
     (test_Y == sampled_Y.float()).sum().item() / len(test_Y) * 100))
