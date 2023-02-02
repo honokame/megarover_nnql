f3a2c51c0452b48074aa4e7fe4983919341097dd
honokame <honoka.happy.0613@icloud.com>
2023-02-02 10:58:26 +0900

    ファイルを入力で決定する

--- nnql/scripts/gcn.py
+++ nnql/scripts/gcn.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#python3 gcn.py 特徴量 訓練番号 テスト番号
 
 import dgl
 import torch 
@@ -13,42 +14,50 @@ from dgl.data import MiniGCDataset
 import dgl.function as fn
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
+
+#データ読み込み
+train_df = 'dataset/r5t8/scan' + sys.argv[1] + '/feature' + sys.argv[1] + '.csv'
+train_pf = 'dataset/statusclass' + sys.argv[1] + '.csv'
+test_df = 'dataset/r5t8/scan' + sys.argv[2] + '/feature' + sys.argv[2] + '.csv'
+test_pf = 'dataset/statusclass' + sys.argv[2] + '.csv'
+model_f = 'dataset/model_r5t8' + sys.argv[1] + sys.argv[2] + '.pth'
 
 #訓練データ読み込み
-train_data = np.loadtxt(fname="data/r9t12/train34_feature.csv",  dtype="int",  delimiter=",")
-train_place = np.loadtxt(fname="data/r9t12/train34_statusclass.csv", dtype="int", delimiter=",")
+train_data = np.loadtxt(fname=train_df,  dtype="int",  delimiter=",")
+train_place = np.loadtxt(fname=train_pf, dtype="int", delimiter=",")
 
 #訓練データグラフ作成
 trainset = []
 for data, place in zip(train_data, train_place):
-  label = place[6]-1#5,6,7                            # ground truth
+  label = place[4]-1#5,6,7                            # ground truth
   data = data.tolist()  
   #feature_list = [data[1:73],data[73:145],data[145:217],data[217:289],data[289:361],data[361:433],data[433:505],data[505:577],data[577:649]] #r9t8 
-  feature_list = [data[1:109],data[109:217],data[217:325],data[325:433],data[433:541],data[541:649],data[649:757],data[757:865],data[865:973]] #r9t12
-  #feature_list = [data[1:41],data[41:81],data[81:121],data[121:161],data[161:201],data[201:241],data[241:281],data[281:321],data[321:361]] #r5t8 
+  #feature_list = [data[1:109],data[109:217],data[217:325],data[325:433],data[433:541],data[541:649],data[649:757],data[757:865],data[865:973]] #r9t12
+  feature_list = [data[1:41],data[41:81],data[81:121],data[121:161],data[161:201],data[201:241],data[241:281],data[281:321],data[321:361]] #r5t8 
   #print(feature_list)
   #print(label)
   g = dgl.graph(([0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8], [1,0,2,1,3,2,4,3,5,4,6,5,7,6,8,7]))      # edge
   g.ndata['feat'] = torch.from_numpy(np.array( feature_list).astype(np.float32)).clone() 
   #print(g)
   trainset.append([g,label])
-  
+print(feature_list)  
 print("train_data:",len(trainset))
 #nx.draw(g.to_networkx().to_undirected(), with_labels=True)
 #plt.show()
 
 #テストデータ読み込み
-test_data = np.loadtxt(fname="data/r9t12/test1_feature.csv",  dtype="int",  delimiter=",")
-test_place = np.loadtxt(fname="data/r9t12/test1_statusclass.csv", dtype="int", delimiter=",")
+test_data = np.loadtxt(fname=test_df,  dtype="int",  delimiter=",")
+test_place = np.loadtxt(fname=test_pf, dtype="int", delimiter=",")
 
 #テストデータグラフ作成
 testset = []
 for data, place in zip(test_data, test_place):
-  label = place[6]-1                            # ground truth
+  label = place[4]-1                            # ground truth
   data = data.tolist() 
   #feature_list = [data[1:73],data[73:145],data[145:217],data[217:289],data[289:361],data[361:433],data[433:505],data[505:577],data[577:649]] #r9t8 
-  feature_list = [data[1:109],data[109:217],data[217:325],data[325:433],data[433:541],data[541:649],data[649:757],data[757:865],data[865:973]]#r9t12
-  #feature_list = [data[1:41],data[41:81],data[81:121],data[121:161],data[161:201],data[201:241],data[241:281],data[281:321],data[321:361]] #r5t8  
+  #feature_list = [data[1:109],data[109:217],data[217:325],data[325:433],data[433:541],data[541:649],data[649:757],data[757:865],data[865:973]]#r9t12
+  feature_list = [data[1:41],data[41:81],data[81:121],data[121:161],data[161:201],data[201:241],data[241:281],data[281:321],data[321:361]] #r5t8  
   #print(feature_list)
   #print(label)
   g = dgl.graph(([0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8], [1,0,2,1,3,2,4,3,5,4,6,5,7,6,8,7]))      # edge
@@ -129,8 +138,8 @@ data_loader = DataLoader(
     shuffle=True,
     collate_fn=collate)
 
-feature = 108  #r9t12:108 r9t8:72 r5:t8:40
-classes = 208  #26,208,312
+feature = 40  #r9t12:108 r9t8:72 r5:t8:40
+classes = 26  #26,208,312
 
 # Create model
 model = Classifier(feature, 256, classes)    #特徴ベクトルの次元、場所クラス数
@@ -139,7 +148,7 @@ optimizer = optim.Adam(model.parameters(),lr=0.001) #0.001
 model.train()
 
 epoch_losses = []
-for epoch in range(50):
+for epoch in range(5):
     epoch_loss = 0
     for iter, (bg, label) in enumerate(data_loader):
         prediction = model(bg)
@@ -153,7 +162,7 @@ for epoch in range(50):
     #if epoch_loss<0.05:
     #  break
     epoch_losses.append(epoch_loss)
-torch.save(model.state_dict(),'data/r9t12/model34_208_e50.pth')
+torch.save(model.state_dict(),model_f)
 
 ####  test ###################################
 model.eval()
